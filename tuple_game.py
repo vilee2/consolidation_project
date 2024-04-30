02c0cba836f6e97926955ed085b4a260b66953c9
Virginia Lee <vilee2@terpmail.umd.edu>
2024-04-30 16:22:19 -0400

    wrote code to ask for player count

---
+++ tuple_game.py
@@ -0,0 +1,18 @@
+
+import random
+
+die = 1, 2, 3, 4, 5, 6
+
+
+
+# asks how many players are playing the game
+player_count = input("How many players are playing?")
+print(player_count)
+
+if player_count == 1:
+    points = {"Player 1" : }
+elif player_count == 2:
+    points = {"Player 1" : , "Player 2" : }
+elif player_count == 3:
+    points = {"Player 1" : , "Player 2" : , "Player 3" : }
+
