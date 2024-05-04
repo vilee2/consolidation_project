873d3e0a19e8f19086cecc17f9c5d9f4271fee33
Virginia Lee <vilee2@terpmail.umd.edu>
2024-05-04 19:26:37 -0400

    Updated the function that simulates dice roll and calculates the points won.

--- tuple_game.py
+++ tuple_game.py
@@ -1,36 +1,47 @@
 
 # this import allows to "roll dice" by randomly selecting a number from die_faces
 import random
+import numpy as np
 
 # asks how many players are playing the game
-player_count = input("How many players are playing?\n")
-print(player_count)
+# player_count = input("How many players are playing?\n")
 
-if player_count == 1:
-    player_points = {"Player 1" : 0}
-elif player_count == 2:
-    player_points = {"Player 1" : 0, "Player 2" : 0}
-elif player_count == 3:
-    player_points = {"Player 1" : 0, "Player 2" : 0, "Player 3" : 0}
 
-die1 = (1, 2, 3, 4, 5, 6)
-die2 = (1, 2, 3, 4, 5, 6)
-die3 = (1, 2, 3, 4, 5, 6)
+player_points = {"Player 1" : 0}
+
+die_faces = (1, 2, 3, 4, 5, 6)
 
 roll_results = []
 roll_counts = {1 : 0, 2 : 0, 3 : 0, 4 : 0, 5 : 0, 6 : 0} 
 
-def dice_roll():
-    for face in range(1):
-        outcome_die1 = random.choice(die1)
-        outcome_die2 = random.choice(die2)
-        outcome_die3 = random.choice(die3)
-    if outcome_die1 == outcome_die2 and outcome_die2 == outcome_die3:
-        points = 0
-        player_points[points] == 0
-        print("You have 'tupled out' so you have 0 points for this turn.")
-    elif outcome_die1 == outcome_die2 or outcome_die1 == outcome_die3 or outcome_die2 == outcome_die3:
+
+def dice_roll(player):
+    """rolls three different dice and assigns points according to the game's rules"""
+    for dice_roll in range(3):
+        this_roll = random.choice(die_faces)
+        roll_results.append(this_roll)
+        roll_counts[this_roll] += 1
+
+    # if all three dice return the same value, you 'tupled out' and earn 0 points
+    if roll_results[0] == roll_results[1] and roll_results[1] == roll_results[2]:
+        print("You have 'tupled out' so you earned 0 points for this turn.")
+        
+    # if the outcome for two out of three dice are the same, those dice are fixed and cannot be re-rolled
+    elif roll_results[0] == roll_results[1] or roll_results[0] == roll_results[2] or roll_results[1] == roll_results[2]:
         print("These two dice have the same value and are 'fixed'. They cannot be re-rolled")
+        if roll_results[0] == roll_results[1] and roll_results[1] != roll_results[2]:
+            reroll = random.choice(die_faces)
+            player_points[reroll] += reroll + roll_results[0] * 2
+        elif roll_results[1] == roll_results[2] and roll_results[1] != roll_results[0]:
+            reroll = random.choice(die_faces)
+            player_points[reroll] += reroll + roll_results[1] * 2
+        elif roll_results[2] == roll_results[0] and roll_results[0] != roll_results[1]:
+            reroll = random.choice(die_faces)
+            player_points[reroll] += reroll + roll_results[2] * 2
+    else:
+        player_points["Player 1"] += sum(roll_results)
+    return print(f"These are the current points: {player_points}")
+
 
 
 
