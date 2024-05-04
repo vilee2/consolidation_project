aa4223ecf24565b41d8adbf9203c53b18d84fa53
Virginia Lee <vilee2@terpmail.umd.edu>
2024-05-04 16:47:19 -0400

    Began the dice roll simulation

--- tuple_game.py
+++ tuple_game.py
@@ -1,18 +1,29 @@
 
 import random
 
-die = 1, 2, 3, 4, 5, 6
-
-
-
 # asks how many players are playing the game
-player_count = input("How many players are playing?")
+player_count = input("How many players are playing?\n")
 print(player_count)
 
 if player_count == 1:
-    points = {"Player 1" : }
+    points = {"Player 1" : 0}
 elif player_count == 2:
-    points = {"Player 1" : , "Player 2" : }
+    points = {"Player 1" : 0, "Player 2" : 0}
 elif player_count == 3:
-    points = {"Player 1" : , "Player 2" : , "Player 3" : }
+    points = {"Player 1" : 0, "Player 2" : 0, "Player 3" : 0}
+
+die_faces = (1, 2, 3, 4, 5, 6)
+
+
+roll_results = []
+roll_counts = {1 : 0, 2 : 0, 3 : 0, 4 : 0, 5 : 0, 6 : 0} 
+for face in range(3):
+    this_roll = random.choice(die_faces)
+    roll_results.append(this_roll)
+    roll_counts[this_roll] += 1
+print(roll_counts)
+
+
+
+
 
